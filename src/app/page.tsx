9d960924b1f23fca8b886b5f7286e9beb9e408d0
Jakub Zarzeczny <jkzarzeczny@gmail.com>
2023-10-29 07:42:39 +0100

    [R-1] Prepare CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,60 @@
+import Link from "next/link";
+import styles from "./page.module.scss";
+import Image from "next/image";
+
 export default function Home() {
-  return <h2>landing page</h2>;
+  return (
+    <section className={styles.cta}>
+      <h1>
+        Make a note
+        <br />
+        and present
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
+        consequatur quasi, vel assumenda, accusamus praesentium quibusdam odio
+      </p>
+      <div className={styles.buttonsContainer}>
+        <Link className={styles.primaryButton} href="/register">
+          Register now
+        </Link>
+        <Link href="/demo" className={styles.secondaryButton}>
+          How it works
+        </Link>
+      </div>
+
+      <Image
+        src={"assets/notes.svg"}
+        alt="note image"
+        width={500}
+        height={500}
+      />
+
+      <div className={styles.bulb}></div>
+      <div className={styles.bulbSmall}>
+        <Image
+          src={"assets/student.svg"}
+          alt="student image"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className={styles.bulbSmall}>
+        <Image
+          src={"assets/notepad.svg"}
+          alt="student image"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className={styles.bulbSmall}>
+        <Image
+          src={"assets/book.svg"}
+          alt="student image"
+          width={50}
+          height={50}
+        />
+      </div>
+    </section>
+  );
 }
--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -32,32 +32,6 @@ export const Header = () => {
           Login
         </Link>
       </header>
-      <div className={styles.headerLanding}>
-        <h1>
-          Make a note
-          <br />
-          and present
-        </h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
-          consequatur quasi, vel assumenda, accusamus praesentium quibusdam odio
-        </p>
-        <div className={styles.buttonsContainer}>
-          <Link className={styles.primaryButton} href="/register">
-            Register now
-          </Link>
-          <Link href="/demo" className={styles.secondaryButton}>
-            How it works
-          </Link>
-        </div>
-
-        <Image
-          src={"assets/notes.svg"}
-          alt="note image"
-          width={500}
-          height={500}
-        />
-      </div>
     </>
   );
 };
